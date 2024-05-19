fdaa83c8b344825543cfa2512101cf9687ce09a5
jagernau <stepanenkov25@gmail.com>
2024-05-19 23:54:04 +0300

    классметод практика

--- classmethod_staticmethod_decorators/practic.py
+++ classmethod_staticmethod_decorators/practic.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+
 """ 
 Классметод:
 """
@@ -25,3 +27,34 @@ class Dog:
         return cls.__sey
 
 print(f"отдача атрибута с помощью классметода: {Dog.get_sey()}")
+
+"3-переназначение атрибутов"
+class Person:
+    def __init__(self, name, age):
+        self.name: str = name
+        self.age: int = age
+
+    @classmethod
+    def calculate_age(cls, name, birth):
+        curent_year = dt.now().year
+        return cls(name, int(curent_year) - birth)
+
+person = Person.calculate_age("Максим", 1988)
+print(f"Имя: {person.name}, Возраст: {person.age}")
+
+"4-наследственность"
+class Family:
+    family_skills = ["Рисование",]
+
+    @classmethod
+    def add_skill(cls, skil):
+        cls.family_skills.append(skil)
+
+class Masha(Family):
+    gender = "Female"
+    name = "Маша"
+
+
+masha = Masha()
+masha.add_skill("Танцы")
+print(f"{masha.name}{masha.family_skills}")
